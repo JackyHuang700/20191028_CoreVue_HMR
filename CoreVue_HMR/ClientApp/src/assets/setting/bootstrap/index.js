08283f082d408c0f01ab95c3b6c022213b3f552d
Jacky <jacky8595125@gmail.com>
2019-10-31 15:20:05 +0800

    add bootstrap-vue

--- CoreVue_HMR/ClientApp/src/assets/setting/bootstrap/index.js
+++ CoreVue_HMR/ClientApp/src/assets/setting/bootstrap/index.js
@@ -1,5 +1,32 @@
-// bootstrap4
-import 'popper.js'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-window.$ = window.jQuery = require('jquery')
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+// const bootstrapArray = {
+//   bootstrap: 1,
+//   bootstrapVue: 2
+// }
+
+// switch (bootstrapArray.bootstrapVue) {
+//   case bootstrapArray.bootstrap:
+//     // bootstrap4
+//     require('popper.js');
+//     require('bootstrap');
+//     require('bootstrap/dist/css/bootstrap.min.css');
+//     window.$ = window.jQuery = require('jquery');
+//     break;
+
+//   case bootstrapArray.bootstrapVue:
+//     // bootstrapVue
+//     import Vue from 'vue';
+//     import BootstrapVue from 'bootstrap-vue';
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+//     Vue.use(BootstrapVue);
+//     break;
+
+//   default:
+//     break;
+// }
