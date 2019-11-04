4bf8401d1f5e922a0052a700eeb8c7f0d3492d11
Jacky <jacky8595125@gmail.com>
2019-11-04 17:37:27 +0800

    delete type commit

--- CoreVue_HMR/ClientApp/src/store/modules/defaultContainer/actions.js
+++ CoreVue_HMR/ClientApp/src/store/modules/defaultContainer/actions.js
@@ -3,12 +3,7 @@
 // -- 命名以 action開頭， example: actionXoooo  --
 import * as types from './mutationsType'
 
-// type commit = {
-//   commit: any
-// }
-
 //
-// export const acitonSetNumber = ({ commit, state, rootState }: commit, number) => {
 export const acitonSetNumber = ({ commit, state, rootState }, number) => {
   commit(types.SET_NUMBER, state.number + number)
 }
