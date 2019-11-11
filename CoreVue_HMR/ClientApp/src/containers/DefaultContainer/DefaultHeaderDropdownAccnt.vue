baded9426e6f7c8b9839f9550031ddb1dd41bd89
Jacky <jacky8595125@gmail.com>
2019-11-11 10:44:32 +0800

    修正圖片路徑問題

--- CoreVue_HMR/ClientApp/src/containers/DefaultContainer/DefaultHeaderDropdownAccnt.vue
+++ CoreVue_HMR/ClientApp/src/containers/DefaultContainer/DefaultHeaderDropdownAccnt.vue
@@ -2,9 +2,9 @@
   <AppHeaderDropdown right no-caret>
     <template slot="header">
       <img
-        src="img/avatars/6.jpg"
+        src="@/assets/setting/coreui/img/avatars/6.jpg"
         class="img-avatar"
-        alt="admin@bootstrapmaster.com" />
+        alt="admin@gmail.com" />
     </template>\
     <template slot="dropdown">
       <b-dropdown-header tag="div" class="text-center"><strong>Account</strong></b-dropdown-header>
